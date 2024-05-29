4e42025f4e8c3a1eb9aab3294c30df3a32ec9faf
Vithun <vithunchandra@gmail.com>
2024-05-29 18:45:27 +0700

    fix bug

--- app/src/main/java/com/mdp/tourisview/data/api/ApiService.kt
+++ app/src/main/java/com/mdp/tourisview/data/api/ApiService.kt
@@ -10,6 +10,7 @@ import okhttp3.RequestBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 import retrofit2.http.Query
@@ -37,7 +38,7 @@ interface ApiService {
         @Query("name") name: String?=null,
     ): List<MockServerDestination>
 
-    @FormUrlEncoded
+    @Multipart
     @POST("uploadDestination")
     suspend fun uploadDestination(
         @Field("name") name: RequestBody,
